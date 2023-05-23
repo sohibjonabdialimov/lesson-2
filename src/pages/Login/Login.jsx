c02ba9ab73ea3cf2a1e4f7d33e7837c0df6cdc69
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2023-05-23 18:30:38 +0500

    ddewd

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -11,29 +11,46 @@ const Login = () => {
   const passwordRef = useRef();
   const onSubmitFunction = (e) => {
     e.preventDefault();
+    if (inputRef.current.value && passwordRef.current.value) {
+      axios
+        .post("https://fakestoreapi.com/auth/login", {
+          username: inputRef.current.value,
+          password: passwordRef.current.value,
+        })
+        .then((data) => {
+          if (data.status === 200) {
+            setToken(data.data);
+            navigate("/");
+            localStorage.setItem("token", JSON.stringify(data.data));
+          }else{
+            throw new Error("Username or passwor error")
+          }
+        })
 
-    axios
-      .post("https://fakestoreapi.com/auth/login", {
-        username: inputRef.current.value,
-        password: passwordRef.current.value,
-      })
-      .then((data) => {
-        if (data.status === 200) {
-          setToken(data.data);
-          navigate("/");
-          localStorage.setItem("token", JSON.stringify(data.data));
-        }
-      })
-
-      .catch((err) => console.log(err));
-  };
+        .catch((err) => alert(err));
+    }else{
+      alert("Please, fill username and password")
+    }
+  }
 
   console.log(token);
   return (
     <div className="login">
       <form onSubmit={(e) => onSubmitFunction(e)}>
-        <input placeholder="Username" ref={inputRef} type="text" name="" id="" />
-        <input placeholder="Password" ref={passwordRef} type="password" name="" id="" />
+        <input
+          placeholder="Username"
+          ref={inputRef}
+          type="text"
+          name=""
+          id=""
+        />
+        <input
+          placeholder="Password"
+          ref={passwordRef}
+          type="password"
+          name=""
+          id=""
+        />
         <button type="submit">SEND</button>
       </form>
     </div>
